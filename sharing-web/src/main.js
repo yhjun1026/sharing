07bd6f6da82e4352d1fd0e750467472fbe0e1905
lihuiming <724684279@qq.com>
2018-04-17 22:27:37 +0800

    新增前端工程

---
+++ sharing-web/src/main.js
@@ -0,0 +1,152 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import store from './store'
+import VueParticles from 'vue-particles'
+import i18n from './lang' // Internationalization
+import * as utils from '@/utils'
+import 'font-awesome/less/font-awesome.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+/*import {
+  Autocomplete,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Popover,
+  Tooltip,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Tag,
+  Icon,
+  Row,
+  Col,
+  Card,
+  Rate,
+  Cascader,
+  Transfer,
+  Loading,
+  MessageBox,
+  Message,
+  Notification
+} from 'element-ui'
+
+Vue.use(Autocomplete)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Popover)
+Vue.use(Tooltip)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Tag)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+Vue.use(Rate)
+Vue.use(Cascader)
+
+Vue.use(Loading.directive)*/
+
+/*Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message*/
+Vue.prototype.$axios = axios
+Vue.use(VueParticles)
+
+// Vue 的全局配置
+Vue.config.productionTip = false
+
+// 上线全局请求配置
+axios.defaults.baseURL = 'api/v1/'
+
+// http request 拦截器
+axios.interceptors.request.use(
+  config => {
+    if (config.method === 'post') {
+      config.headers['X-CSRF-TOKEN'] = Cookies.get('XSRF-TOKEN') // 让每个请求携带token--['X-Token']为自定义key 请根据实际情况自行修改
+    }
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        // 如果后端返回没有权限
+        case 401:
+          // 清除token信息并跳转到登录页面
+          Cookies.set('isLogin', '0')
+          router.replace('/signin')
+      }
+    }
+    return Promise.reject(error.response.data) // 返回接口返回的错误信息
+  }
+)
+
+new Vue({
+  el: '#app',
+  i18n,
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
