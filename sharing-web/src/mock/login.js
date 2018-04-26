333e7f05062a98e653bb2935315f8180df40a571
yhjun1026 <460342015@qq.com>
2018-04-26 22:41:23 +0800

    180426 yhj

--- sharing-web/src/mock/login.js
+++ sharing-web/src/mock/login.js
@@ -1,91 +1,93 @@
 import Mock from 'mockjs'
 let debug = 1;
 if (debug) {
-    Mock.mock(/login/, {
-        "data": {
-            userId: "@integer(1,10)",
-            "userName|1": ["鲁班七号", "典韦", "蔡文姬", "王昭君", "妲己","铠"]
+  Mock.mock(/login/, {
+    "data": {
+      userId: "@integer(1,10)",
+      "userName|1": ["鲁班七号", "典韦", "蔡文姬", "王昭君", "妲己", "铠"]
+    },
+    "msg": "Success",
+    "status": "YQ-000"
+  })
+  Mock.mock(/getMenu/, {
+    "data": {
+      menu: [
+        // {
+        //   name: '项目管理',
+        //   name_en: 'Projects',
+        //   router: '',
+        //   icon: 'el-icon-time',
+        //   children: [{
+        //     name: '开发备忘',
+        //     name_en: 'Notes',
+        //     router: '/project-info',
+        //     icon: 'el-icon-date'
+        //   }, {
+        //     name: '关于作者',
+        //     name_en: 'About',
+        //     router: '/enroll-list',
+        //     icon: 'el-icon-document'
+        //   }]
+        // }, {
+        //   name: '天气预报',
+        //   name_en: 'Weather',
+        //   router: '/weather',
+        //   icon: 'el-icon-picture-outline',
+        //   children: []
+        // }, {
+        //   name: '魔幻立方',
+        //   name_en: 'Cube',
+        //   router: '/cube',
+        //   icon: 'el-icon-menu',
+        //   children: []
+        // },
+         {
+          name: '权限测试',
+          name_en: 'Authority',
+          router: '/authority-test',
+          icon: 'el-icon-setting',
+          children: []
+        }, {
+          name: '物品管理',
+          name_en: 'Articles',
+          router: '/article-new',
+          icon: 'el-icon-time',
+          children: [{
+            name: '物品新增',
+            name_en: 'NewArticle',
+            router: '/article-new',
+            icon: 'el-icon-date'
+          }, {
+            name: '物品列表',
+            name_en: 'ArticleList',
+            router: '/article-list',
+            icon: 'el-icon-document'
+          }]
         },
-        "msg": "Success",
-        "status": "YQ-000"
-    })
-    Mock.mock(/getMenu/, {
-        "data": {
-            menu: [{
-                name: '项目管理',
-                name_en: 'Projects',
-                router: '',
-                icon: 'el-icon-time',
-                children: [{
-                    name: '开发备忘',
-                    name_en: 'Notes',
-                    router: '/project-info',
-                    icon: 'el-icon-date'
-                }, {
-                    name: '关于作者',
-                    name_en: 'About',
-                    router: '/enroll-list',
-                    icon: 'el-icon-document'
-                }]
-            }, {
-                name: '天气预报',
-                name_en: 'Weather',
-                router: '/weather',
-                icon: 'el-icon-picture-outline',
-                children: []
-            }, {
-                name: '魔幻立方',
-                name_en: 'Cube',
-                router: '/cube',
-                icon: 'el-icon-menu',
-                children: []
-            },{
-                name: '权限测试',
-                name_en: 'Authority',
-                router: '/authority-test',
-                icon: 'el-icon-setting',
-                children: []
-            },{
-              name: '物品管理',
-              name_en: 'Articles',
-              router: '/article-new',
-              icon: 'el-icon-time',
-              children: [{
-                name: '物品新增',
-                name_en: 'NewArticle',
-                router: '/article-new',
-                icon: 'el-icon-date'
-              }, {
-                name: '物品列表',
-                name_en: 'ArticleList',
-                router: '/article-list',
-                icon: 'el-icon-document'
-              }]
-            },
-              {
-                name: '租用记录',
-                name_en: 'RentList',
-                router: '/article-list',
-                icon: 'el-icon-setting',
-                children: []
-              },
-              {
-                name: '物品归还',
-                name_en: 'RentList',
-                router: '/article-list',
-                icon: 'el-icon-setting',
-                children: []
-              },
-              {
-                name: '物品赔偿',
-                name_en: 'RentList',
-                router: '/article-list',
-                icon: 'el-icon-setting',
-                children: []
-              },
-            ]
+        {
+          name: '租用记录',
+          name_en: 'RentList',
+          router: '/article-list',
+          icon: 'el-icon-setting',
+          children: []
         },
-        "msg": "Success",
-        "status": "YQ-000"
-    })
+        {
+          name: '物品归还',
+          name_en: 'RentList',
+          router: '/article-list',
+          icon: 'el-icon-setting',
+          children: []
+        },
+        {
+          name: '物品赔偿',
+          name_en: 'RentList',
+          router: '/article-list',
+          icon: 'el-icon-setting',
+          children: []
+        },
+      ]
+    },
+    "msg": "Success",
+    "status": "YQ-000"
+  })
 }
