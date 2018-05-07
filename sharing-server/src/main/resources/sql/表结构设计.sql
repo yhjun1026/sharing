86f3a3e714cf49990291632a55bfdefd13c56992
zengnali <nayumi@126.com>
2018-05-07 23:58:08 +0800

    物品借用单表结构调整

--- sharing-server/src/main/resources/sql/表结构设计.sql
+++ sharing-server/src/main/resources/sql/表结构设计.sql
@@ -57,59 +57,63 @@ MEMO            VARCHAR(256)   NULL                   /*备注(预留)*/
 );
 
 /*物品借用单主表*/
-CREATE TABLE SHGoodsBorrowMst(
-UUID            VARCHAR(32)    PRIMARY KEY NOT NULL,
-BILLNUMBER      VARCHAR(32)    NOT NULL,              /*物品借用单号*/
-STAT            VARCHAR(16)    NOT NULL,              /*单据状态 new 新增; 已领取 using; 已归还 backed; 已赔偿 compensated*/
-BORROWERTYPE    VARCHAR(64)    NULL,                  /*借用人类型(内部员工，外部)*/ 
-BORROWER        VARCHAR(64)    NULL,                  /*借用人*/
-MOBILE          VARCHAR(32)    NULL,                  /*借用人手机号*/
-ADDRESS         VARCHAR(256)   NULL,                  /*借用人联系地址*/
-RETURNTIME      DATETIME       NULL,                  /*计划归还时间*/
-REALRETURNTIME  DATETIME       NULL,                  /*实际归还时间*/
-FILLER          VARCHAR(64)    NULL,                  /*填单人*/  
-FILDATE         DATETIME       NULL,                  /*填单人时间*/  
-LASTER          VARCHAR(64)    NULL,                  /*最后修改人*/
-LASTUPDTIME     DATETIME       NULL,                  /*最后更新时间*/ 
-MEMO            VARCHAR(256)   NULL,                  /*备注(预留)*/
-STOREUUID       VARCHAR(32)    NULL                   /*所属组织(SHSTORE.UUID)*/
-);
+CREATE TABLE IF NOT EXISTS `shgoodsborrowmst` (
+  `uuid` varchar(32) NOT NULL,
+  `bill_number` varchar(32) NOT NULL COMMENT '借用单据号',
+  `stat` varchar(16) NOT NULL COMMENT '单据状态 new 新增; 已领取 using; 已归还 backed; 已赔偿 compensated',
+  `borrow_type` varchar(64) DEFAULT NULL COMMENT '借用人类型(内部员工，外部)，暂时无用',
+  `borrower` varchar(64) DEFAULT NULL COMMENT '借用人',
+  `mobile` varchar(32) NOT NULL COMMENT '借用人手机号',
+  `address` varchar(256) DEFAULT NULL COMMENT '借用人联系地址',
+  `plan_return_time` datetime NOT NULL COMMENT '计划归还时间',
+  `real_return_time` datetime DEFAULT NULL COMMENT '实际归还时间',
+  `borrow_operator` varchar(64) DEFAULT NULL COMMENT '领用受理人',
+  `borrow_operate_time` datetime DEFAULT NULL COMMENT '领用受理时间',
+  `return_operator` varchar(64) DEFAULT NULL COMMENT '归还受理人',
+  `return_operate_time` datetime DEFAULT NULL COMMENT '归还受理时间',
+  `memo` varchar(256) DEFAULT NULL COMMENT '备注',
+  `store_uuid` varchar(32) DEFAULT NULL COMMENT '所属组织，暂时无用',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='物品借用单主表';
 
 /*物品借用单明细*/
-CREATE TABLE SHGoodsBorrowDtl(
-UUID               VARCHAR(32)      PRIMARY KEY NOT NULL,
-GOODSUUID          VARCHAR(32)      NOT NULL,              /*物品UUID*/
-GOODSCODE          VARCHAR(64)      NOT NULL,              /*物品代码*/
-GOODSNAME          VARCHAR(64)      NOT NULL,              /*物品名称*/
-BORROWQTY          DECIMAL(10,2)    DEFAULT 0 NULL,        /*物品借用数量*/
-BORROWDESCRIP      VARCHAR(512)     NULL,                  /*借出物品情况描述*/
-DEPOSITAMT         DECIMAL(10,2)    DEFAULT 0 NULL,        /*借用时缴纳押金*/
-DAMAGED            INT              DEFAULT 0 NULL,        /*损坏标识;0 没有损坏;1 已损坏;*/
-RETURNDESCRIP      VARCHAR(512)     NULL,                  /*归还物品情况描述*/
-BACKQTY            DECIMAL(10,2)    DEFAULT 0 NULL,        /*物品归还数量*/
-PAYAMT             DECIMAL(10,2)    DEFAULT 0 NULL,        /*使用费用金额*/
-BACKDEPOSITAMT     DECIMAL(10,2)    DEFAULT 0 NULL,        /*归还押金情况*/
-MEMO               VARCHAR(256)     NULL,                  /*备注(预留)*/
-BORROWUUID         VARCHAR(32)      NULL,                  /*借用单ID(SHGoodsBorrowMst.UUID)*/
-BORROWPAYUUID      VARCHAR(32)      NULL,                  /*借用单付款id*/
-BACKPAYUUID        VARCHAR(32)      NULL,                  /*退款付款id*/
-COMPENSATEPAYUUID  VARCHAR(32)      NULL                   /*赔偿付款id*/
-);
+CREATE TABLE IF NOT EXISTS `shgoodsborrowdtl` (
+  `uuid` varchar(32) NOT NULL,
+  `goods_uuid` varchar(32) NOT NULL COMMENT '物品id',
+  `goods_code` varchar(64) DEFAULT NULL COMMENT '物品代码',
+  `goods_name` varchar(64) DEFAULT NULL COMMENT '物品名称',
+  `borrow_qty` decimal(10,2) DEFAULT '0.00' COMMENT '物品借用数量，暂时无用',
+  `borrow_descrip` varchar(512) DEFAULT NULL COMMENT '借出物品情况描述',
+  `deposit_amt` decimal(10,2) DEFAULT '0.00' COMMENT '借用物品时缴纳押金',
+  `damaged` int(11) DEFAULT '0' COMMENT '损坏标识;0 没有损坏;1 已损坏;',
+  `return_descrip` varchar(512) DEFAULT NULL COMMENT '物品归还时描述',
+  `return_qty` decimal(10,2) DEFAULT '0.00' COMMENT '物品归还数量，暂时无用',
+  `pay_amt` decimal(10,2) DEFAULT '0.00' COMMENT '使用费用金额',
+  `return_deposit_amt` decimal(10,2) DEFAULT '0.00' COMMENT '归还押金金额',
+  `borrow_uuid` varchar(32) NOT NULL COMMENT '借用单主表id',
+  `borrow_pay_uuid` varchar(32) DEFAULT NULL COMMENT '借用付款id',
+  `return_pay_uuid` varchar(32) DEFAULT NULL COMMENT '归还时退款id',
+  `compensate_pay_uuid` varchar(32) DEFAULT NULL COMMENT '赔偿付款id',
+  `memo` varchar(256) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`uuid`),
+  KEY `borrow_uuid` (`borrow_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='借用单明细表';
 
 /*支付信息*/
-CREATE TABLE SHPAYINFO(
-UUID               VARCHAR(32)      PRIMARY KEY NOT NULL,
-PAYID              VARCHAR(64)      NOT NULL,              /*支付记录id*/
-TRANSTYPE          INT              NOT NULL,              /*交易类型,  1:支付， -1:退款*/
-PAYTYPE            INT              NULL,                  /*支付方式, 1:微信， 2:支付宝*/
-TRADENO            VARCHAR(64)      NOT NULL,              /*支付单号（支付平台）*/
-OUTTRADENO         VARCHAR(64)      NOT NULL,              /*支付单号（商户）*/
-PAYAMT             DECIMAL(10,2)    DEFAULT 0 NULL,        /*使用费用金额*/
-PAYUSER            VARCHAR(64)      NOT NULL,              /*支付人*/
-PAYTIME            DATETIME         NULL,                  /*支付（退款）时间*/
-ORITRADENO         VARCHAR(64)      NULL,                  /*原支付单号（支付平台，退款时使用）*/
-ORIOUTTRADENO      VARCHAR(64)      NULL                   /*原支付单号（商户，退款时使用）*/
-);
+CREATE TABLE IF NOT EXISTS `shpayinfo` (
+  `uuid` varchar(32) NOT NULL,
+  `pay_id` varchar(64) DEFAULT NULL COMMENT '支付记录id',
+  `trans_type` int(11) NOT NULL COMMENT '交易类型,  1:支付， -1:退款',
+  `pay_type` int(11) DEFAULT NULL COMMENT '支付方式, 1:微信， 2:支付宝',
+  `trade_no` varchar(64) NOT NULL COMMENT '支付单号（支付平台）',
+  `out_trade_no` varchar(64) NOT NULL COMMENT '支付单号（商户）',
+  `pay_amt` decimal(10,2) DEFAULT '0.00' COMMENT '支付金额',
+  `pay_user` varchar(64) DEFAULT NULL COMMENT '支付人',
+  `pay_time` datetime DEFAULT NULL COMMENT '支付（退款）时间',
+  `ori_trade_no` varchar(64) DEFAULT NULL COMMENT '原支付单号（支付平台，退款时使用）',
+  `ori_out_trade_no` varchar(64) DEFAULT NULL COMMENT '原支付单号（商户，退款时使用）',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='付款信息表';
 
 CREATE TABLE IF NOT EXISTS `serial_number` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
